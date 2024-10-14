dcf9b610f73639915f02e96296043105f1e6d64b
Angelo Konara <angelokonara04@gmail.com>
2024-10-14 14:15:50 +0200

    Changed gitignore and the usage of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     mode: 'development',
@@ -7,14 +9,44 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
+        filename: '[name][contenthash].js',
+        clean: true,
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Hacker News',
+            filename: 'index.html',
+            template: 'src/template.html',
+        }),
+//        new BundleAnalyzerPlugin()
+    ]
 }
